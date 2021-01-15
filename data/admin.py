3ae5c5155a1fea370a27e7f73bece312592de424
rstodden <regina.stodden@uni-duesseldorf.de>
2021-01-15 15:13:42 +0100

    register models for admin sites

--- data/admin.py
+++ data/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Sentence, Document, Corpus
+from .models import Sentence, Document, Corpus, Token, DocumentPair
 
 admin.site.register(Sentence)
 admin.site.register(Document)
 admin.site.register(Corpus)
+admin.site.register(Token)
+admin.site.register(DocumentPair)
--- rating/admin.py
+++ rating/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Rating
+from .models import Rating, Transformation
 
 # Register your models here.
 
 # admin.site.register(Pair)
 admin.site.register(Rating)
+admin.site.register(Transformation)
