1b9d71a91955edbcbc427e3604b3afb6bb01ca60
rstodden <regina.stodden@uni-duesseldorf.de>
2021-02-04 14:37:54 +0100

    change log priority

--- change_log/forms.py
+++ change_log/forms.py
@@ -5,5 +5,5 @@ from .models import ChangeLog
 class AddChange(forms.ModelForm):
     class Meta:
         model = ChangeLog
-        fields = ["headline", "topic", "comment"]
+        fields = ["headline", "topic", "comment", "priority"]
 
--- change_log/models.py
+++ change_log/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import TS_annotation_tool.utils
 
 
 # Create your models here.
@@ -16,3 +17,6 @@ class ChangeLog(models.Model):
 	headline = models.TextField(max_length=288)
 	created_at = models.DateTimeField(auto_now_add=True)
 	finished_at = models.DateTimeField(null=True, blank=True)
+	priority = TS_annotation_tool.utils.IntegerRangeField(min_value=1, max_value=5, blank=True, null=True,
+														  help_text="Select 1 for highest priority and 5 for lowest priority.")
+
